263df78889b402da9da87c822a03499208b9c97d
Aishakhan08 <nk7704970119@gmail.com>
2026-07-12 11:21:50 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -39,7 +39,7 @@ def get_base64(file):
     with open(file, "rb") as f:
         return base64.b64encode(f.read()).decode()
 
-img = get_base64("assets/hospital_bg.jpg")
+img = get_base64("hospital_bg.jpg")
 
 if "login" not in st.session_state:
     st.session_state.login = False
